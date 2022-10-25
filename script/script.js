27f2f5bb8aa337757f6ee7654feee55f3e715c89
Oursel06 <alexoursel6@gmail.com>
2022-10-25 19:13:48 +0200

    config page d'accueil + transition profil

--- script/script.js
+++ script/script.js
@@ -1,19 +1,47 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 500){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 500) {
             $('.up').addClass("show");
         }
-        else{
+        else {
             $('.up').removeClass("show");
         }
     });
 
-    $('.up').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.up').click(function () {
+        $('html').animate({ scrollTop: 0 });
     });
 
-    $('.menu-btn').click(function(){
-       $('.nav .ul').toggleClass("active");
-       $('.menu-btn i').toggleClass("bi bi-list bi bi-x");
+    $('.menu-btn').click(function () {
+        $('.nav .ul').toggleClass("active");
+        $('.menu-btn i').toggleClass("bi-list bi-x");
     });
+
+    let toogle = 0
+    $('#theme').click(function () {
+        $('#theme').toggleClass("bi-sun bi-moon").animate(1000);
+        if (toogle === 0) {
+            // $(this).animate({ backgroundText: "black" }, 1000);
+            document.documentElement.style.setProperty('--background-profil', 'aliceblue',);
+            document.documentElement.style.setProperty('--background-home', 'white');
+            document.documentElement.style.setProperty('--main-text', 'black');
+            document.documentElement.style.setProperty('--secondary-text', 'darksalmon');
+            toogle++;
+        }
+        else {
+            document.documentElement.style.setProperty('--background-profil', 'black');
+            document.documentElement.style.setProperty('--background-home', '#191919');
+            document.documentElement.style.setProperty('--main-text', 'white');
+            document.documentElement.style.setProperty('--secondary-text', 'royalblue');
+
+            toogle--;
+        }
+    })
+
+    $('#accueil_decouvrir').click(function () {
+        $('#accueil').hide(100);
+        // let s = document.getElementById("portfolio");
+        // s.style.display = "block";
+        $(".portfolio").show(500);
+    })
 })
